9c8a77da7ac6ae25663b4bac9d81457e13354b80
mewteebee <96594141+mewteebee@users.noreply.github.com>
2022-10-11 12:08:54 +0300

    Initial commit, first ruby-on-rails project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show", as: :article
+  root "articles#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
