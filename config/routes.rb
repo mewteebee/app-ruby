06da505d5a81f82fb4b077cd02cdccbee5b0498c
mewteebee <96594141+mewteebee@users.noreply.github.com>
2022-10-11 12:18:07 +0300

    Added 'new blog' functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles/new", to: "articles#new"
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show", as: :article
   root "articles#index"
